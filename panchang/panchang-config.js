5ec6687521df580d66d1144fa16c2a3fe7766654
Mahadevastrologerma108 <256489137+Mahadevastrologerma108@users.noreply.github.com>
2026-02-20 09:16:24 +0545

    Update panchang-config.js

--- panchang/panchang-config.js
+++ panchang/panchang-config.js
@@ -3,13 +3,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAsS...", // Apni asli API Key yahan rehne dena
-    authDomain: "mahadev-astrologer-ma.firebaseapp.com",
-    databaseURL: "https://mahadev-astrologer-ma-default-rtdb.firebaseio.com",
-    projectId: "mahadev-astrologer-ma",
-    storageBucket: "mahadev-astrologer-ma.appspot.com",
-    messagingSenderId: "353...",
-    appId: "1:353...:web:..."
+    const firebaseConfig = {
+  apiKey: "AIzaSyAgcfrzQm6wezgtU5Q5BP8wxXatmoWqYrw",
+  authDomain: "mahadev-astrologer.firebaseapp.com",
+  databaseURL: "https://mahadev-astrologer-default-rtdb.firebaseio.com",
+  projectId: "mahadev-astrologer",
+  storageBucket: "mahadev-astrologer.firebasestorage.app",
+  messagingSenderId: "559664802739",
+  appId: "1:559664802739:web:4285f4dc461f570cc2b9c6"
 };
 
 const app = initializeApp(firebaseConfig);
